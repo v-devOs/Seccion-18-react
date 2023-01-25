7daab5456c3b30621243f6c31083ea25fe36ffca
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-01-25 13:40:23 -0600

    Primer parte de RTK query

---
+++ src/TodoApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const TodoApp = () => {
+  return (
+    <>
+      <h1>Todos - RTK Query</h1>
+      <hr />
+      <h4>isLoading...</h4>
+
+      <pre>...</pre>
+
+      <button>
+        NextTodo
+      </button>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import App from './App'
 import './index.css'
 import { PokemonApp } from './PokemonApp'
 import { store } from './store/store'
+import { TodoApp } from './TodoApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={ store }>
-      <PokemonApp />
+      <TodoApp />
 
     </Provider>
   </React.StrictMode>,
